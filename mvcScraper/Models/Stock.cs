685d8b4ae6ce94e56dd859f9eadb34d69e8e5abf
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-11-29 20:29:55 -0500

    added display annotation to the stock field titles

--- mvcScraper/Models/Stock.cs
+++ mvcScraper/Models/Stock.cs
@@ -1,14 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace mvcScraper.Models
 {
     public class Stock
     {
         public string Symbol { get; set; }
+        
+        [Display(Name = "Last Price")]
         public string LastPrice { get; set; }
         public string Change { get; set; }
         public string Currency { get; set; }
+        
+        [Display(Name = "Scrape Date")]
         public System.DateTime DataCollectedOn { get; set; }
         public string Volume { get; set; }
+        
+        [Display(Name = "Average Volume")]
         public string AvgVol3m { get; set; }
+        
+        [Display(Name = "Market Cap")]
         public string MarketCap { get; set; }
       
         
